80a885e572737ff8dc129a3bd0aaa11304aee526
huangzd <930211680@qq.com>
2018-06-10 17:06:58 +0800

    图片

--- learnConcurrency/src/main/java/com/learnConcurrency/executor/cachedThreadPool/Main.java
+++ learnConcurrency/src/main/java/com/learnConcurrency/executor/cachedThreadPool/Main.java
@@ -7,5 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
         ExecutorService pool = Executors.newCachedThreadPool();
+        while (true){
+            pool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
     }
 }
--- learnConcurrency/src/main/java/com/learnConcurrency/executor/fixedThreadPool/Main.java
+++ learnConcurrency/src/main/java/com/learnConcurrency/executor/fixedThreadPool/Main.java
@@ -1,11 +1,27 @@
 package com.learnConcurrency.executor.fixedThreadPool;
 
+import com.sun.xml.internal.bind.v2.model.annotation.RuntimeAnnotationReader;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        while (true){
+            pool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
     }
 }
