953d38dd4997e5458f53f79b453dd4eef08fccb8
930211680@qq.com <930211680@qq.com>
2018-07-17 17:23:12 +0800

    hash命令项目案例新增

---
+++ learnRedis/case-hash/src/main/java/com/learnRedis/hash/CaseHashApplication.java
@@ -0,0 +1,12 @@
+package com.learnRedis.hash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseHashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseHashApplication.class, args);
+    }
+}
