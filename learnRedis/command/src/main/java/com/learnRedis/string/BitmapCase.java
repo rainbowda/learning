ba3f155f1740e0055dc52211df1b04f9a87598b9
huangzd <930211680@qq.com>
2018-07-18 21:17:33 +0800

    统计连续上线代码修改

--- learnRedis/command/src/main/java/com/learnRedis/string/BitmapCase.java
+++ learnRedis/command/src/main/java/com/learnRedis/string/BitmapCase.java
@@ -8,39 +8,19 @@ import java.util.concurrent.*;
 
 public class BitmapCase extends RedisBaseConnection {
 
+    //存储的key前缀
+    private static final String ONLINE_KEY_PREFIX = "online:";
     //天数
     private static final int DAY_NUM = 30;
     //用户数量
     private static final int PEOPLE_NUM = 10000;
-
-    private CountDownLatch countDownLatch = new CountDownLatch(DAY_NUM);
 
     @Test
     public void daliyActive() {
         /**
          *模拟数据
          */
-
-        int poolSize = Runtime.getRuntime().availableProcessors() * 2;
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(poolSize, poolSize, 60, TimeUnit.SECONDS, new ArrayBlockingQueue(30));
-        //DAY_NUM天
-        for (int i = 1; i <= DAY_NUM; i++) {
-            int finalI = i;
-            executor.execute(() -> {
-                //假设有PEOPLE_NUM个用户
-                for (int j = 0; j < PEOPLE_NUM; j++) {
-                    redisTemplate.opsForValue().setBit("active:" + finalI, j, Math.random() > 0.1);
-                }
-                countDownLatch.countDown();
-            });
-        }
-
-        //等待线程全部执行完成
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        createData();
 
         /**
          * 开始统计
@@ -56,19 +36,52 @@ public class BitmapCase extends RedisBaseConnection {
 
         //30
         calActive(30);
+    }
 
+    /**
+     * 生成模拟数据
+     */
+    public void createData() {
+        //用来保证线程执行完在进行后面的操作
+        CountDownLatch countDownLatch = new CountDownLatch(DAY_NUM);
+
+        int poolSize = Runtime.getRuntime().availableProcessors() * 2;
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(poolSize, poolSize, 60, TimeUnit.SECONDS, new ArrayBlockingQueue(DAY_NUM-poolSize));
+        //DAY_NUM天
+        for (int i = 1; i <= DAY_NUM; i++) {
+            int finalI = i;
+            executor.execute(() -> {
+                //假设有PEOPLE_NUM个用户
+                for (int j = 1; j <= PEOPLE_NUM; j++) {
+                    redisTemplate.opsForValue().setBit(ONLINE_KEY_PREFIX + finalI, j, Math.random() > 0.1);
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        //等待线程全部执行完成
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * 根据传入的天数统计
+     *
      * @param day
      */
     public void calActive(int day) {
+        if (day < 0 || day > DAY_NUM){
+            throw new IllegalArgumentException("传入的天数不能小于0或者大于30天!");
+        }
+
         long calStart = System.currentTimeMillis();
         BitSet active = new BitSet();
         active.set(0, PEOPLE_NUM);
         for (int i = 1; i <= day; i++) {
-            BitSet bitSet = BitSet.valueOf(jedis.get(("active:" + i).getBytes()));
+            BitSet bitSet = BitSet.valueOf(jedis.get((ONLINE_KEY_PREFIX + i).getBytes()));
             active.and(bitSet);
         }
         long calEnd = System.currentTimeMillis();
