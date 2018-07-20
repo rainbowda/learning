195d54b9ee93eba46a9a8587c25a82408bc44dd5
930211680@qq.com <930211680@qq.com>
2018-07-20 17:26:49 +0800

    list案例

---
+++ learnRedis/case-list/src/main/java/com/learnRedis/list/CaseListApplication.java
@@ -0,0 +1,12 @@
+package com.learnRedis.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseListApplication.class, args);
+    }
+}
