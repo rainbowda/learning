93b1bad9a8e38b3b9706df0ad1e476d210ebd23d
930211680@qq.com <930211680@qq.com>
2018-07-20 17:27:31 +0800

    sortedset案例

---
+++ learnRedis/case-sortedset/src/main/java/com/learnRedis/sortedset/CaseSortedSetApplication.java
@@ -0,0 +1,12 @@
+package com.learnRedis.sortedset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseSortedSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseSortedSetApplication.class, args);
+    }
+}
