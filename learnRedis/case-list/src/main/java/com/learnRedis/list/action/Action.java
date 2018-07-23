2c937894bed805f4a090b1d9fc77e63031c29d34
930211680@qq.com <930211680@qq.com>
2018-07-23 17:36:37 +0800

    list案例代码

--- learnRedis/case-list/src/main/java/com/learnRedis/list/action/Action.java
+++ learnRedis/case-list/src/main/java/com/learnRedis/list/action/Action.java
@@ -1,20 +1,24 @@
 package com.learnRedis.list.action;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/list")
 public class Action {
 
-    private static final String MY_LOG_REDIS_KEY_PREFIX = ":";
-    private static final String MY_LOG_REDIS_ID_KEY = "ID";
+    private static final String LIST_KEY = "list:1";
 
     private RedisTemplate redisTemplate;
+
+    private ListOperations listOperations;
 
     @Autowired(required = false)
     public void setRedisTemplate(RedisTemplate redisTemplate) {
@@ -24,8 +28,60 @@ public class Action {
         redisTemplate.setHashKeySerializer(stringSerializer);
         redisTemplate.setHashValueSerializer(stringSerializer);
         this.redisTemplate = redisTemplate;
+        this.listOperations = this.redisTemplate.opsForList();
+
     }
 
+    /**
+     * 列表
+     * @return
+     */
+    @RequestMapping(value = "/getList",method = RequestMethod.GET)
+    public List getList(){
+        List list = listOperations.range(LIST_KEY, 0, -1);
 
+        //可以用size获取成员长度
+        //listOperations.size(LIST_KEY);
+
+        return list;
+    }
+
+    /**
+     * 向队列的左边添加值
+     * @return
+     */
+    @RequestMapping(value = "/leftPush",method = RequestMethod.POST)
+    public boolean leftPush(@RequestBody JSONObject value){
+        listOperations.leftPush(LIST_KEY,value.toString());
+        return true;
+    }
+
+    /**
+     * 向队列的右边添加值
+     * @return
+     */
+    @RequestMapping(value = "/rightPush",method = RequestMethod.POST)
+    public boolean rightPush(@RequestBody JSONObject value){
+        listOperations.rightPush(LIST_KEY,value.toString());
+        return true;
+    }
+
+    /**
+     * 向队列的左边弹出值
+     * @return
+     */
+    @RequestMapping(value = "/leftPop",method = RequestMethod.GET)
+    public Object leftPop(){
+        return listOperations.leftPop(LIST_KEY);
+    }
+
+    /**
+     * 向队列的右边弹出值
+     * @return
+     */
+    @RequestMapping(value = "/rightPop",method = RequestMethod.GET)
+    public Object rightPop(){
+        return listOperations.rightPop(LIST_KEY);
+    }
 
 }
--- learnRedis/case-list/src/main/resources/static/js/index.js
+++ learnRedis/case-list/src/main/resources/static/js/index.js
@@ -2,14 +2,92 @@ new Vue({
     el: '#app',
     data () {
         return {
-
+            title:"",
+            modalVisible:false,
+            submitLoading:false,
+            pushType:"",
+            form:{
+                data:""
+            },
+            formValidate:{
+                data: [
+                    { required: true, message: '请输入内容', trigger: 'blur' }
+                ],
+            },
+            listData:[],
+            listDataSize:0,
+            popListData:[],
+            columns: [
+                {
+                    title: 'data',
+                    key: 'data'
+                },
+            ],
         }
     },
     methods: {
         init(){
-
+            this.listData = [];
+            this.listDataSize = 0;
+            this.getList();
         },
-
+        getList(){
+            axios.get('/list/getList')
+                .then(res=>{
+                    this.listData = res.data;
+                    this.listDataSize = this.listData.length;
+                })
+        },
+        openLeftPushModel(){
+            this.pushType = "leftPush";
+            this.modalVisible = true;
+        },
+        openRightPushModel(){
+            this.pushType = "rightPush";
+            this.modalVisible = true;
+        },
+        cancel(){
+            this.modalVisible = false;
+        },
+        submit(){
+            this.$refs.form.validate(valid => {
+                if (valid) {
+                    this.submitLoading = true;
+                    axios.post('/list/'+this.pushType,this.form)
+                        .then(res=>{
+                            this.submitLoading = false;
+                            if(res.data == true){
+                                this.$Notice.success({
+                                    desc: '新增队列成功'
+                                });
+                                this.modalVisible = false;
+                                this.form.data = "";
+                            } else {
+                                this.$Notice.warning({
+                                    desc: '添加队列失败'
+                                });
+                            }
+                            this.init();
+                        })
+                }
+            });
+        },
+        leftPop(){
+            axios.get('/list/leftPop')
+                .then(res=>{
+                    this.$Message.success('弹出数据：'+JSON.stringify(res.data));
+                    this.popListData.push(res.data);
+                    this.init();
+                })
+        },
+        rightPop(){
+            axios.get('/list/rightPop')
+                .then(res=>{
+                    this.$Message.success('弹出数据：'+JSON.stringify(res.data));
+                    this.popListData.push(res.data);
+                    this.init();
+                })
+        },
     },
     mounted(){
         this.init();
