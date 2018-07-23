29ae32310e4b3fa80acb78a804ecc50920c5d30f
huangzd <930211680@qq.com>
2018-07-23 21:20:03 +0800

    约瑟夫问题代码修改

--- learnRedis/command/src/main/java/com/learnRedis/list/JosephusProblem.java
+++ learnRedis/command/src/main/java/com/learnRedis/list/JosephusProblem.java
@@ -6,24 +6,23 @@ import org.junit.Test;
 public class JosephusProblem extends RedisBaseConnection {
 
     @Test
-   public void test(){
+    public void test() {
         //构造数据
         for (int i = 1; i <= 41; i++) {
-            listOperations.leftPush("josephus",String.valueOf(i));
+            listOperations.leftPush("josephus", String.valueOf(i));
         }
 
         int index = 1;
-        while (listOperations.size("josephus") > 0){
-            System.out.println(listOperations.range("josephus", 0 ,-1));
-
+        while (listOperations.size("josephus") > 0) {
             //当数到3时，弹出
-            if (index == 3){
-                System.out.println(listOperations.rightPop("josephus"));
+            if (index == 3) {
+                System.out.println(listOperations.range("josephus", 0, -1));
+                System.out.println("当前被杀的人是：" + listOperations.rightPop("josephus"));
                 index = 0;
             } else {
                 listOperations.rightPopAndLeftPush("josephus", "josephus");
             }
             index++;
         }
-   }
+    }
 }
