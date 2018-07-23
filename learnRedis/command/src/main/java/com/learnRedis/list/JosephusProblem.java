9f3c3a9b893dbc3154f6fd5a426a5a7641a2fc1f
930211680@qq.com <930211680@qq.com>
2018-07-23 17:36:52 +0800

    约瑟夫问题代码

---
+++ learnRedis/command/src/main/java/com/learnRedis/list/JosephusProblem.java
@@ -0,0 +1,29 @@
+package com.learnRedis.list;
+
+import com.learnRedis.base.RedisBaseConnection;
+import org.junit.Test;
+
+public class JosephusProblem extends RedisBaseConnection {
+
+    @Test
+   public void test(){
+        //构造数据
+        for (int i = 1; i <= 41; i++) {
+            listOperations.leftPush("josephus",String.valueOf(i));
+        }
+
+        int index = 1;
+        while (listOperations.size("josephus") > 0){
+            System.out.println(listOperations.range("josephus", 0 ,-1));
+
+            //当数到3时，弹出
+            if (index == 3){
+                System.out.println(listOperations.rightPop("josephus"));
+                index = 0;
+            } else {
+                listOperations.rightPopAndLeftPush("josephus", "josephus");
+            }
+            index++;
+        }
+   }
+}
