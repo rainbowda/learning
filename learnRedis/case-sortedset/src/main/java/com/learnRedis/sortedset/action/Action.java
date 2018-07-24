f6522caddc90b12bdf259622ad31616afe12d429
930211680@qq.com <930211680@qq.com>
2018-07-24 17:44:50 +0800

    sortedset案例

--- learnRedis/case-sortedset/src/main/java/com/learnRedis/sortedset/action/Action.java
+++ learnRedis/case-sortedset/src/main/java/com/learnRedis/sortedset/action/Action.java
@@ -1,20 +1,27 @@
 package com.learnRedis.sortedset.action;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisZSetCommands;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/zset")
 public class Action {
 
-    private static final String MY_LOG_REDIS_KEY_PREFIX = ":";
-    private static final String MY_LOG_REDIS_ID_KEY = "ID";
+    private static final String ZSET_KEY = "articleList";
 
     private RedisTemplate redisTemplate;
+
+    private ZSetOperations zSetOperations;
 
     @Autowired(required = false)
     public void setRedisTemplate(RedisTemplate redisTemplate) {
@@ -24,8 +31,66 @@ public class Action {
         redisTemplate.setHashKeySerializer(stringSerializer);
         redisTemplate.setHashValueSerializer(stringSerializer);
         this.redisTemplate = redisTemplate;
+        this.zSetOperations = redisTemplate.opsForZSet();
+
     }
 
+    /**
+     * 列表
+     *
+     * @return
+     */
+    @RequestMapping(value = "/getList/{sortType}", method = RequestMethod.GET)
+    public Set getList(@PathVariable String sortType) {
+        //如果没有数据，则添加10条数据
+        if (zSetOperations.size(ZSET_KEY) == 0){
+            for (int i = 1; i <= 10; i++) {
+                zSetOperations.add(ZSET_KEY,"文章:"+i, (int)(Math.random()*10+i));
+            }
+        }
+
+        //ASC根据分数从小到大排序,DESC反之
+        if ("ASC".equals(sortType)){
+            return zSetOperations.rangeWithScores(ZSET_KEY, 0, -1);
+        } else {
+            return zSetOperations.reverseRangeWithScores(ZSET_KEY, 0, -1);
+        }
+    }
+
+
+    /**
+     * 赞或踩
+     * @param member
+     * @param type
+     * @return
+     */
+    @RequestMapping(value = "/star", method = RequestMethod.POST)
+    public boolean starOrUnStar(String member, String type) {
+        if ("UP".equals(type)){
+            zSetOperations.incrementScore(ZSET_KEY, member, 1);
+        } else {
+            zSetOperations.incrementScore(ZSET_KEY, member, -1);
+        }
+        return true;
+    }
+
+    /**
+     * 获取排名
+     * @param member
+     * @param type
+     * @return
+     */
+    @RequestMapping(value = "/rank/{type}/{member}", method = RequestMethod.GET)
+    public Long rank(@PathVariable String member, @PathVariable String type) {
+        Long rank = null;
+        if ("ASC".equals(type)){
+            rank = zSetOperations.rank(ZSET_KEY, member);
+        } else {
+            rank = zSetOperations.reverseRank(ZSET_KEY, member);
+        }
+
+        return rank;
+    }
 
 
 }
--- learnRedis/case-sortedset/src/main/resources/static/js/index.js
+++ learnRedis/case-sortedset/src/main/resources/static/js/index.js
@@ -2,14 +2,52 @@ new Vue({
     el: '#app',
     data () {
         return {
+            sortType:"ASC",
+            sortName:"降序显示",
+            listData:[],
 
         }
     },
     methods: {
         init(){
-
+            this.getList();
         },
+        getList(){
+            axios.get('/zset/getList/'+this.sortType)
+                .then(res=>{
+                    this.listData = res.data;
+                })
+        },
+        changeSortType(){
+            if (this.sortType == "ASC"){
+                this.sortType = "DESC";
+                this.sortName = "升序显示";
+            } else {
+                this.sortType = "ASC";
+                this.sortName = "降序显示";
+            }
+            this.getList();
+        },
+        star(type, member){
+            var params = new URLSearchParams();
+            params.append('type', type);
+            params.append('member', member);
 
+            axios.post('/zset/star',params)
+                .then(res=>{
+                    this.$Message.success('操作成功');
+                    this.init();
+                })
+        },
+        rank(type, member){
+
+            axios.get('/zset/rank/'+type+"/"+member)
+                .then(res=>{
+                    console.log(res);
+                    this.$Message.success(member+'的排名:'+res.data);
+                    this.init();
+                })
+        },
     },
     mounted(){
         this.init();
