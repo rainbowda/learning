0ae3f8abc97ec4a6814cc64b626ba6b488284d62
930211680@qq.com <930211680@qq.com>
2018-07-24 13:39:49 +0800

    set案例代码

--- learnRedis/case-set/src/main/java/com/learnRedis/set/action/Action.java
+++ learnRedis/case-set/src/main/java/com/learnRedis/set/action/Action.java
@@ -2,19 +2,28 @@ package com.learnRedis.set.action;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/set")
 public class Action {
 
-    private static final String MY_LOG_REDIS_KEY_PREFIX = ":";
-    private static final String MY_LOG_REDIS_ID_KEY = "ID";
+    private static final String A_FRIEND_KEY = "friend:a";
+
+    private static final String B_FRIEND_KEY = "friend:b";
 
     private RedisTemplate redisTemplate;
+
+    private SetOperations setOperations;
 
     @Autowired(required = false)
     public void setRedisTemplate(RedisTemplate redisTemplate) {
@@ -24,8 +33,85 @@ public class Action {
         redisTemplate.setHashKeySerializer(stringSerializer);
         redisTemplate.setHashValueSerializer(stringSerializer);
         this.redisTemplate = redisTemplate;
+        this.setOperations = redisTemplate.opsForSet();
     }
 
+    /**
+     * 列表
+     *
+     * @return
+     */
+    @RequestMapping(value = "/getList", method = RequestMethod.GET)
+    public Map getList() {
+        Map map = new HashMap();
 
+        Set aFriend = setOperations.members(A_FRIEND_KEY);
+        Set bFriend = setOperations.members(B_FRIEND_KEY);
+
+        map.put("aFriend", aFriend);
+        map.put("bFriend", bFriend);
+
+        return map;
+    }
+
+    /**
+     * 添加好友
+     *
+     * @return
+     */
+    @RequestMapping(value = "/addFriend", method = RequestMethod.POST)
+    public Long addFriend(String user, String friend) {
+        String currentKey = A_FRIEND_KEY;
+        if ("B".equals(user)) {
+            currentKey = B_FRIEND_KEY;
+        }
+        //返回添加成功的条数
+        return setOperations.add(currentKey, friend);
+    }
+
+    /**
+     * 删除好友
+     *
+     * @return
+     */
+    @RequestMapping(value = "/delFriend", method = RequestMethod.DELETE)
+    public Long delFriend(String user, String friend) {
+        String currentKey = A_FRIEND_KEY;
+        if ("B".equals(user)) {
+            currentKey = B_FRIEND_KEY;
+        }
+        //返回删除成功的条数
+        return setOperations.remove(currentKey, friend);
+    }
+
+    /**
+     * 共同好友(交集)
+     *
+     * @return
+     */
+    @RequestMapping(value = "/intersectFriend", method = RequestMethod.GET)
+    public Set intersectFriend() {
+        return setOperations.intersect(A_FRIEND_KEY, B_FRIEND_KEY);
+    }
+
+    /**
+     * 返回用户独有的好友(差集)
+     *
+     * @return
+     */
+    @RequestMapping(value = "/differenceFriend", method = RequestMethod.GET)
+    public Set differenceFriend(String user) {
+        return setOperations.difference(A_FRIEND_KEY, B_FRIEND_KEY);
+    }
+
+    /**
+     * 返回所有的好友(并集)
+     *
+     * @return
+     */
+    @RequestMapping(value = "/unionFriend", method = RequestMethod.GET)
+    public Set unionFriend() {
+        return setOperations.union(A_FRIEND_KEY, B_FRIEND_KEY);
+    }
 
 }
--- learnRedis/case-set/src/main/resources/static/js/index.js
+++ learnRedis/case-set/src/main/resources/static/js/index.js
@@ -2,14 +2,114 @@ new Vue({
     el: '#app',
     data () {
         return {
-
+            aFriendData: [],
+            bFriendData: [],
+            resultFrient:[],
+            modalVisible:false,
+            submitLoading:false,
+            resultFrientTitle:"",
+            form:{
+                user:"",
+                friend:""
+            },
+            formValidate:{
+                user: [
+                    { required: true, message: '请选择用户', trigger: 'blur' }
+                ],
+                friend: [
+                    { required: true, message: '请输入好友名称', trigger: 'blur' }
+                ],
+            },
         }
     },
     methods: {
         init(){
-
+            this.resultFrientTitle = "";
+            this.getList();
         },
+        getList(){
+            axios.get('/set/getList')
+                .then(res=>{
+                    this.aFriendData = res.data.aFriend;
+                    this.bFriendData = res.data.bFriend;
+                })
+        },
+        addFriend(){
+            this.modalVisible = true;
+            this.form.friend = "";
+        },
+        cancel(){
+            this.modalVisible = false;
+        },
+        submit(){
+            this.$refs.form.validate(valid => {
+                if (valid) {
+                    this.submitLoading = true;
+                    var params = new URLSearchParams();
+                    params.append('user', this.form.user);
+                    params.append('friend', this.form.friend);
 
+                    axios.post('/set/addFriend',params)
+                        .then(res=>{
+                            this.submitLoading = false;
+                            if(res.data > 0){
+                                this.$Notice.success({
+                                    desc: '添加好友成功'
+                                });
+                                this.modalVisible = false;
+                                this.form.data = "";
+                            } else {
+                                this.$Notice.warning({
+                                    desc: '已经有该好友'
+                                });
+                            }
+                            this.init();
+                        })
+                }
+            });
+        },
+        delFriend(user, friend){
+            this.$Modal.confirm({
+                title: '删除',
+                content: '你确定要删除'+friend+"?",
+                onOk: () => {
+                    axios.delete('/set/delFriend',{params:{"user":user,"friend":friend}})
+                        .then(res=>{
+                            if(res.data > 0){
+                                this.$Notice.success({
+                                    desc: '删除成功'
+                                });
+                            } else {
+                                this.$Notice.warning({
+                                    desc:'删除失败'
+                                });
+                            }
+                            this.init();
+                        })
+                }
+            });
+        },
+        intersectFriend(){
+            this.resultFrientTitle = "共同好友";
+            axios.get('/set/intersectFriend')
+                .then(res=>{
+                    this.resultFrient = res.data;
+                })
+        },
+        differenceFriend(){
+            this.resultFrientTitle = "用户独有的好友";
+            axios.get('/set/differenceFriend')
+                .then(res=>{
+                    this.resultFrient = res.data;
+                })
+        },
+        unionFriend(){
+            this.resultFrientTitle = "所有的好友";
+            axios.get('/set/unionFriend')
+                .then(res=>{
+                    this.resultFrient = res.data;
+                })
+        },
     },
     mounted(){
         this.init();
