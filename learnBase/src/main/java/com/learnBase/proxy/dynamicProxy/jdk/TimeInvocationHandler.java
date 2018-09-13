c936247ae597477642c7bd71e0f145f96261f138
930211680@qq.com <930211680@qq.com>
2018-09-13 15:15:58 +0800

    jdk动态代理

--- learnBase/src/main/java/com/learnBase/proxy/dynamicProxy/jdk/TimeInvocationHandler.java
+++ learnBase/src/main/java/com/learnBase/proxy/dynamicProxy/jdk/TimeInvocationHandler.java
@@ -17,7 +17,7 @@ public class TimeInvocationHandler implements InvocationHandler {
         Object result = method.invoke(target, args);
 
         long endTime = System.currentTimeMillis();
-        System.out.println("执行"+method.getName()+"方法,耗时"+(endTime - startTime)+"毫秒");
+        System.out.println("使用jdk代理，执行"+method.getName()+"方法,耗时"+(endTime - startTime)+"毫秒");
 
         return result;
     }
