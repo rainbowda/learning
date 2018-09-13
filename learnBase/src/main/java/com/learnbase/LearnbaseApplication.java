4ecbf9f888e034e177cdf9edb14ec159c9afd43a
930211680@qq.com <930211680@qq.com>
2018-09-13 13:58:55 +0800

    基础项目初始化

---
+++ learnBase/src/main/java/com/learnbase/LearnbaseApplication.java
@@ -0,0 +1,12 @@
+package com.learnbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnbaseApplication.class, args);
+    }
+}
