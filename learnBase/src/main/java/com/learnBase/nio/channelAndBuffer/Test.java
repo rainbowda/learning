1930100d3ae58167556645f1afb4968962bc0d90
930211680@qq.com <930211680@qq.com>
2018-09-14 14:52:22 +0800

    channel buffer

---
+++ learnBase/src/main/java/com/learnBase/nio/channelAndBuffer/Test.java
@@ -0,0 +1,35 @@
+package com.learnBase.nio.channelAndBuffer;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class Test {
+    public static void main(String[] args) {
+        String filePath = Test.class.getResource("testChannelAndBuffer.txt").getPath();
+
+        try (RandomAccessFile file = new RandomAccessFile(filePath,"rw")) {
+
+            FileChannel channel = file.getChannel();
+
+            //容量为48
+            ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+
+            //读取
+            while (channel.read(byteBuffer) != -1){
+                //设置buffer为读模式
+                byteBuffer.flip();
+
+                //是否读完
+                while (byteBuffer.hasRemaining()){
+                    System.out.print((char)byteBuffer.get());
+                }
+
+                //清空已经读取的部分
+                byteBuffer.clear();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
