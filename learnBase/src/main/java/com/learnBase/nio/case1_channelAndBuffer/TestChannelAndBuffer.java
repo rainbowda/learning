9e96ea95ce5bf1308bcbb74db1c2db07fd239aff
930211680@qq.com <930211680@qq.com>
2018-09-14 15:57:36 +0800

    channel buffer

--- learnBase/src/main/java/com/learnBase/nio/case1_channelAndBuffer/TestChannelAndBuffer.java
+++ learnBase/src/main/java/com/learnBase/nio/case1_channelAndBuffer/TestChannelAndBuffer.java
@@ -8,9 +8,8 @@ public class TestChannelAndBuffer {
     public static void main(String[] args) {
         String filePath = TestChannelAndBuffer.class.getResource("testChannelAndBuffer.txt").getPath();
 
-        try (RandomAccessFile file = new RandomAccessFile(filePath,"rw")) {
-
-            FileChannel channel = file.getChannel();
+        try (RandomAccessFile file = new RandomAccessFile(filePath,"rw");
+             FileChannel channel = file.getChannel()) {
 
             //容量为48
             ByteBuffer byteBuffer = ByteBuffer.allocate(48);
@@ -28,6 +27,7 @@ public class TestChannelAndBuffer {
                 //清空已经读取的部分
                 byteBuffer.clear();
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
