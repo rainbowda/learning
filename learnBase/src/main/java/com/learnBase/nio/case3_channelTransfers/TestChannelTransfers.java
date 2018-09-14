a9035abd87686743e8364da8b45e48d801d47291
930211680@qq.com <930211680@qq.com>
2018-09-14 15:50:19 +0800

    channel transfer

---
+++ learnBase/src/main/java/com/learnBase/nio/case3_channelTransfers/TestChannelTransfers.java
@@ -0,0 +1,25 @@
+package com.learnBase.nio.case3_channelTransfers;
+
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+
+public class TestChannelTransfers {
+    public static void main(String[] args) {
+        String fromPath = TestChannelTransfers.class.getResource("fromFile.txt").getPath();
+        String toPath = TestChannelTransfers.class.getResource("toFile.txt").getPath();
+
+
+        try (RandomAccessFile fromFile = new RandomAccessFile(fromPath,"rw");
+             RandomAccessFile toFile = new RandomAccessFile(toPath,"rw")) {
+
+            FileChannel fromchannel = fromFile.getChannel();
+            FileChannel toChannel = toFile.getChannel();
+
+            toChannel.transferTo(0,fromchannel.size(),fromchannel);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
