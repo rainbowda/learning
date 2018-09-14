658158221b4a1b3d0a624bd0adf002d9f4f9b7ba
930211680@qq.com <930211680@qq.com>
2018-09-14 17:13:42 +0800

    socket and serverSocket

---
+++ learnBase/src/main/java/com/learnBase/nio/case4_socketAndServerSocket/TestSocketAndServerSocket.java
@@ -0,0 +1,18 @@
+package com.learnBase.nio.case4_socketAndServerSocket;
+
+import org.junit.Test;
+
+public class TestSocketAndServerSocket {
+
+    @Test
+    public void socketServer(){
+
+    }
+
+    @Test
+    public void socketClient(){
+
+    }
+
+
+}
