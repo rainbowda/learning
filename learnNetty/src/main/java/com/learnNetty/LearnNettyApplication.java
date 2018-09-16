9ac4d9cb38c9129544e079642384bcfa5f7afd76
huangzd <930211680@qq.com>
2018-09-16 10:18:18 +0800

    netty项目初始化

---
+++ learnNetty/src/main/java/com/learnNetty/LearnNettyApplication.java
@@ -0,0 +1,12 @@
+package com.learnNetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnNettyApplication.class, args);
+    }
+}
