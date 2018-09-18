ddf8e93cb0012c8fccc49b14518b9b825f775314
930211680@qq.com <930211680@qq.com>
2018-09-18 15:35:30 +0800

    常量类

---
+++ learnNetty/src/main/java/com/learnNetty/Constant.java
@@ -0,0 +1,11 @@
+package com.learnNetty;
+
+public class Constant {
+
+    public final static String ADDRESS_LOCALHOST = "localhost";
+    public final static Integer PORT_10001 = 10001, PORT_10002 = 10002, PORT_10003 = 10003,
+                                PORT_10004 = 10004, PORT_10005 = 10005, PORT_10006 = 10006,
+                                PORT_10007 = 10007, PORT_10008 = 10008, PORT_10009 = 10009;
+
+    private Constant() {}
+}
