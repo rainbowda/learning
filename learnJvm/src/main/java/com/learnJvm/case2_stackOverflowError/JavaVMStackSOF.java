14c55caf5616b55e13cd373e545e97002f901eb0
930211680@qq.com <930211680@qq.com>
2018-09-19 13:25:43 +0800

    sof异常代码

---
+++ learnJvm/src/main/java/com/learnJvm/case2_stackOverflowError/JavaVMStackSOF.java
@@ -0,0 +1,26 @@
+package com.learnJvm.case2_stackOverflowError;
+
+/**
+ * VM Args：-Xss128k
+ * @author zzm
+ */
+public class JavaVMStackSOF {
+
+	private int stackLength = 1;
+
+	public void stackLeak() {
+		stackLength++;
+		stackLeak();
+	}
+
+	public static void main(String[] args) throws Throwable {
+		JavaVMStackSOF oom = new JavaVMStackSOF();
+		try {
+			oom.stackLeak();
+		} catch (Throwable e) {
+			System.out.println("stack length:" + oom.stackLength);
+			throw e;
+		}
+	}
+}
+
