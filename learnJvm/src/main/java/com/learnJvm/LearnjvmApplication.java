381a352e5754e84644811172275dc3f11e4d0916
930211680@qq.com <930211680@qq.com>
2018-09-19 09:59:55 +0800

    jvm项目初始化

---
+++ learnJvm/src/main/java/com/learnJvm/LearnjvmApplication.java
@@ -0,0 +1,12 @@
+package com.learnJvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnjvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnjvmApplication.class, args);
+    }
+}
