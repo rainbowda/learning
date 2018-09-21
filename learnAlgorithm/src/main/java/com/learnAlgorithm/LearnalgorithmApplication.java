0cf31df67fa7a729f217c10b81b745e3f58bbac7
huangzd <930211680@qq.com>
2018-09-21 20:27:20 +0800

    项目新建

---
+++ learnAlgorithm/src/main/java/com/learnAlgorithm/LearnalgorithmApplication.java
@@ -0,0 +1,12 @@
+package com.learnAlgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnalgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnalgorithmApplication.class, args);
+    }
+}
