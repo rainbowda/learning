87ed9d00e23bd07e65228316779806c42f7916c9
huangzd <930211680@qq.com>
2018-09-26 19:32:50 +0800

    最简单的实现思路

---
+++ learnAlgorithm/src/main/java/com/learnAlgorithm/topK/Case1.java
@@ -0,0 +1,17 @@
+package com.learnAlgorithm.topK;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class Case1 {
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+
+        return null;
+    }
+
+
+}
