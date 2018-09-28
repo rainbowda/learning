a40cce9c53ebf48fb61f86591fd90d1f3cdb5f1f
huangzd <930211680@qq.com>
2018-09-28 20:04:05 +0800

    冒k次获取Topk

---
+++ learnAlgorithm/src/main/java/com/learnAlgorithm/topK/Case2.java
@@ -0,0 +1,16 @@
+package com.learnAlgorithm.topK;
+
+import java.util.List;
+
+/**
+ * 冒k次获取Topk
+ */
+public class Case2 {
+
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+
+        return null;
+    }
+
+
+}
