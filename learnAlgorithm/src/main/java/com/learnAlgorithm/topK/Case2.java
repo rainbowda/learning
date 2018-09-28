8e1cc65702fb35e006413735662725660050ba97
huangzd <930211680@qq.com>
2018-09-28 20:29:41 +0800

    冒泡案例代码修改

--- learnAlgorithm/src/main/java/com/learnAlgorithm/topK/Case2.java
+++ learnAlgorithm/src/main/java/com/learnAlgorithm/topK/Case2.java
@@ -13,7 +13,7 @@ public class Case2 {
     public static List<Integer> topKFrequent(int[] nums, int k) {
         int i, j;
         for(i=0; i<k; i++){//表示n次排序过程。
-            for(j=1; j<k-i; j++){
+            for(j=1; j<nums.length-i; j++){
                 if(nums[j-1] > nums[j]){//前面的数字大于后面的数字就交换
                     //交换a[j-1]和a[j]
                     int temp;
@@ -26,13 +26,13 @@ public class Case2 {
 
         List<Integer> list = Arrays.asList(ArrayUtils.toObject(nums));
 
-        return list.subList(0,k);
+        return list.subList(nums.length-k,nums.length);
     }
 
     public static void main(String[] args) {
         int[] nums = {8, 3, 1, 2, 5, 2, 0};
 
-        List<Integer> integers = Case1.topKFrequent(nums, 3);
+        List<Integer> integers = Case2.topKFrequent(nums, 3);
 
         System.out.println(integers);
     }
