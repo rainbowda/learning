6b48e6cabd7178371588fdc814f405483550417c
rainbowda <930211680@qq.com>
2018-10-02 14:14:32 +0800

    随机选择代码实现

--- learnAlgorithm/src/main/java/com/learnAlgorithm/topK/Case4.java
+++ learnAlgorithm/src/main/java/com/learnAlgorithm/topK/Case4.java
@@ -12,11 +12,37 @@ public class Case4 {
 
     public static List<Integer> topKFrequent(int[] nums, int k) {
 
-        //使用小
+
 
 
         List<Integer> list = Arrays.asList(ArrayUtils.toObject(nums));
         return list.subList(0,k);
+    }
+
+    private int randommizedSelect(int[] nums, int low, int high, int k){
+        if (low == high) {
+            return nums[low];
+        }
+        //计算当前利用一次快速排序获取nums[low]在整个数组中的位置
+        int i = partition(nums, low, high);
+
+        //如果nums[low]位置小于k，说明k的元素在数组的右边，反之，则在左边
+        if ((i-low) >= k){
+            return randommizedSelect(nums, low,i-1,k);
+        } else {
+            return randommizedSelect(nums, i+1,high,k);
+        }
+    }
+
+    /**
+     * 一次快速排序代码（未实现）
+     * @param nums
+     * @param low
+     * @param high
+     * @return
+     */
+    private int partition(int[] nums, int low, int high) {
+        return 0;
     }
 
     public static void main(String[] args) {
